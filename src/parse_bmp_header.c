b23c067f75ae3d0e11c36e0453cd625510d171a1
Antoine FONCK <antoinefonck98@gmail.com>
2019-10-28 16:32:08 +0100

    full norme for libbmp

--- src/parse_bmp_header.c
+++ src/parse_bmp_header.c
@@ -1,92 +1,107 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   parse_bmp_header.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: afonck <afonck@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/10/28 15:26:06 by afonck            #+#    #+#             */
+/*   Updated: 2019/10/28 16:18:58 by afonck           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "libbmp.h"
 #include "libft.h"
 #include <errno.h>
 
-static int  read_reserved_bmp(int fd, t_bmp_header *bmp_header, int *offset, char *file)
+static int	read_reserved_bmp(int fd, t_bmp_header *bmp_header, int *offset, \
+char *file)
 {
-    int ret;
+	int ret;
 
-    ret = 0;
-    if ((ret = read(fd, &bmp_header->reserved1, 2)) <= 0)
-        return (bmpheader_readerror(ret, file));
-    if (bmp_header->reserved1 != 0)
-        return (bmp_header_error("two bytes at offset %d should be 0, but are %{r}d\n", *offset, bmp_header->reserved1));
-    *offset += ret;
-    if ((ret = read(fd, &bmp_header->reserved2, 2)) <= 0)
-        return (bmpheader_readerror(ret, file));
-    if (bmp_header->reserved2 != 0)
-        return (bmp_header_error("two bytes @ offset %d should be 0, but are %{r}d\n", *offset, bmp_header->reserved2));
-    *offset += ret;
-    return (EXIT_SUCCESS);
+	ret = 0;
+	if ((ret = read(fd, &bmp_header->reserved1, 2)) <= 0)
+		return (bmpheader_readerror(ret, file));
+	if (bmp_header->reserved1 != 0)
+		return (bmp_header_error(ERRBMP_RES, *offset, bmp_header->reserved1));
+	*offset += ret;
+	if ((ret = read(fd, &bmp_header->reserved2, 2)) <= 0)
+		return (bmpheader_readerror(ret, file));
+	if (bmp_header->reserved2 != 0)
+		return (bmp_header_error(ERRBMP_RES, *offset, bmp_header->reserved2));
+	*offset += ret;
+	return (EXIT_SUCCESS);
 }
 
-int         fill_bmp_header(t_bmp_header *bmp_header, int fd, char *file, int *offset)
+int			fill_bmp_header(t_bmp_header *bmp_header, int fd, char *file, \
+int *offset)
 {
-    int ret;
+	int ret;
 
-    if ((ret = read(fd, &bmp_header->type, 2)) <= 0)
-        return (bmpheader_readerror(ret, file));
-    if (bmp_header->type != 0x4d42) //424d in little endian --> first byte (42=B) is the smallest, second byte is bigger ((4d=M) * 256)
-        return (bmp_header_error("file ID is %{r}d, it should be %{g}d\n", bmp_header->type, BMP_ID));//('M' * 256) + 'B');
-    *offset += ret;
-    if ((ret = read(fd, &bmp_header->size, 4)) <= 0)
-        return (bmpheader_readerror(ret, file));
-    if (bmp_header->size != BMP_SIZE)//<= 0)
-        return (bmp_header_error("wrong size @ index %{b}d: %{r}d\n", *offset, bmp_header->size));
-    *offset += ret;
-    if (read_reserved_bmp(fd, bmp_header, offset, file) != EXIT_SUCCESS)
-        return (EXIT_FAILURE);
-    if ((ret = read(fd, &bmp_header->offset, 4)) <= 0)
-        return (bmpheader_readerror(ret, file));
-    if (bmp_header->offset != OFFSET_BEFORE_DATA)//54)
-        return (bmp_header_error("offset to image data %{r}d @ index %{b}d wrong\n", bmp_header->offset, *offset));
-    *offset += ret;
-    return (EXIT_SUCCESS);
+	if ((ret = read(fd, &bmp_header->type, 2)) <= 0)
+		return (bmpheader_readerror(ret, file));
+	if (bmp_header->type != 0x4d42)
+		return (bmp_header_error(ERRBMP_ID, bmp_header->type, BMP_ID));
+	*offset += ret;
+	if ((ret = read(fd, &bmp_header->size, 4)) <= 0)
+		return (bmpheader_readerror(ret, file));
+	if (bmp_header->size != BMP_SIZE)
+		return (bmp_header_error(ERRBMP_SIZE, *offset, bmp_header->size));
+	*offset += ret;
+	if (read_reserved_bmp(fd, bmp_header, offset, file) != EXIT_SUCCESS)
+		return (EXIT_FAILURE);
+	if ((ret = read(fd, &bmp_header->offset, 4)) <= 0)
+		return (bmpheader_readerror(ret, file));
+	if (bmp_header->offset != OFFSET_BEFORE_DATA)
+		return (bmp_header_error(ERRBMP_OFFSET, bmp_header->offset, *offset));
+	*offset += ret;
+	return (EXIT_SUCCESS);
 }
 
-static int  check_info_header(t_info_header *info_header)
+static int	check_info_header(t_info_header *info_header)
 {
-    if (info_header->size != 40)
-        return(info_header_error("info header size: %{r}d, expected: 40\n", info_header->size));
-    if (info_header->width != PIX_WIDTH) //128 * 128 ?
-        return(info_header_error("width in header: %{r}d is wrong\n", info_header->width));
-    if (info_header->height != PIX_HEIGHT) //128 * 128 ?
-        return(info_header_error("height in header %{r}d is wrong\n", info_header->height));
-    if (info_header->planes != 1)
-        return(info_header_error("format error, color planes: %d\n", info_header->planes));
-    if (info_header->bits != BITS_PER_PIX) //&& info_header->bits != 8) //only accept 24 bit bmp ?
-        return(info_header_error("nb bits per pixel: %d, only 24 accepted\n", info_header->bits));
-    if (info_header->compression != 0)
-        return(info_header_error("compression: %{r}d, only uncompressed bmp are handled\n", info_header->compression));
-    if (info_header->imagesize != IMG_SIZE)
-        return(info_header_error("uncorrect image size: %{r}d\n", info_header->imagesize));
-    if (info_header->xresolution <= 0)
-        return(info_header_error("wrong horizontal res: %{r}d \n", info_header->xresolution));
-    if (info_header->yresolution <= 0)
-        return(info_header_error("uncorrect vertical resolution: %{r}d pixels per meter\n", info_header->yresolution));
-    if (info_header->ncolours != 0)
-        return(info_header_error("uncorrect number of colors in image: %{r}d\n", info_header->ncolours));
-    if (info_header->importantcolours != 0)
-        return(info_header_error("uncorrect number of important colors: %{r}d\n", info_header->importantcolours));
-    return (EXIT_SUCCESS);
+	if (info_header->size != 40)
+		return (info_header_error(ERRBMP_HDSIZE, info_header->size));
+	if (info_header->width != PIX_WIDTH)
+		return (info_header_error(ERRBMP_W_IMG, info_header->width));
+	if (info_header->height != PIX_HEIGHT)
+		return (info_header_error(ERRBMP_H_IMG, info_header->height));
+	if (info_header->planes != 1)
+		return (info_header_error(ERRBMP_CLR_PLN, info_header->planes));
+	if (info_header->bits != BITS_PER_PIX)
+		return (info_header_error(ERRBMP_BBP, info_header->bits));
+	if (info_header->compression != 0)
+		return (info_header_error(ERRBMP_COMPR, info_header->compression));
+	if (info_header->imagesize != IMG_SIZE)
+		return (info_header_error(ERRBMP_IMGSIZE, info_header->imagesize));
+	if (info_header->xresolution <= 0)
+		return (info_header_error(ERRBMP_XRES, info_header->xresolution));
+	if (info_header->yresolution <= 0)
+		return (info_header_error(ERRBMP_YRES, info_header->yresolution));
+	if (info_header->ncolours != 0)
+		return (info_header_error(ERRBMP_NBCOLOR, info_header->ncolours));
+	if (info_header->importantcolours != 0)
+		return (info_header_error(ERRBMP_NBCLI, info_header->importantcolours));
+	return (EXIT_SUCCESS);
 }
 
-int         fill_info_header(t_info_header *info_header, int fd, char *file, int *offset)
+int			fill_info_header(t_info_header *info_header, int fd, \
+char *file, int *offset)
 {
-    int ret;
+	int ret;
 
-    if ((ret = read(fd, info_header, sizeof(t_info_header))) == -1)
-    {
-        ft_dprintf(STDERR_FILENO, "read fail: %{r}s\n", strerror(errno));
-        return (EXIT_FAILURE);
-    }
-    else if (ret == 0)
-    {
-        ft_dprintf(STDERR_FILENO, "unexpected end of file in %{r}s\n", file);
-        return (EXIT_FAILURE);
-    }
-    *offset += sizeof(t_info_header);
-    if (check_info_header(info_header) != EXIT_SUCCESS)
-        return (EXIT_FAILURE);
-    return (EXIT_SUCCESS);
+	if ((ret = read(fd, info_header, sizeof(t_info_header))) == -1)
+	{
+		ft_dprintf(STDERR_FILENO, "read fail: %{r}s\n", strerror(errno));
+		return (EXIT_FAILURE);
+	}
+	else if (ret == 0)
+	{
+		ft_dprintf(STDERR_FILENO, "unexpected end of file in %{r}s\n", file);
+		return (EXIT_FAILURE);
+	}
+	*offset += sizeof(t_info_header);
+	if (check_info_header(info_header) != EXIT_SUCCESS)
+		return (EXIT_FAILURE);
+	return (EXIT_SUCCESS);
 }
--- src/utils.c
+++ src/utils.c
@@ -1,23 +1,32 @@
-//#include "myBMPparser.h"
-//#include "libft.h"
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   utils.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: afonck <afonck@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/10/28 15:27:01 by afonck            #+#    #+#             */
+/*   Updated: 2019/10/28 16:22:57 by afonck           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-int     is_bmp(char *file)
+int	is_bmp(char *file)
 {
-    int len;
+	int len;
 
-    len = 0;
-    while (file[len])
-        len++;
-    len -= 1;
-    if (len < 4)
-        return (0);
-    if (file[len--] != 'p')
-        return (0);
-    if (file[len--] != 'm')
-        return (0);
-    if (file[len--] != 'b')
-        return (0);
-    if (file[len] != '.')
-        return (0);
-    return (1);
+	len = 0;
+	while (file[len])
+		len++;
+	len -= 1;
+	if (len < 4)
+		return (0);
+	if (file[len--] != 'p')
+		return (0);
+	if (file[len--] != 'm')
+		return (0);
+	if (file[len--] != 'b')
+		return (0);
+	if (file[len] != '.')
+		return (0);
+	return (1);
 }
